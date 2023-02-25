01ceda2ecb8e05bcfbc14ac8aa11f50e8ae92220
Ridi133 <121939922+Ridi133@users.noreply.github.com>
2023-02-25 14:08:14 +0600

    Add files via upload

---
+++ DZ1_Alikhan_Alybaev/main.js
@@ -0,0 +1,10 @@
+var askAauestion = prompt("Оцените приложение от 1 до 10")
+if(askAauestion > 5){
+    alert('Спасибо за высокую оценку!')
+}
+else if(askAauestion <= 5){
+    alert('Мы работаем над улучшением сервиса!')
+}
+else{
+    alert("Используйте материалы с урока")
+}
